e1191460204d2912309d26d0d647e8607cef4ef6
gethin.yan <gethin.yan@gmail.com>
2018-11-22 09:30:19 +0800

    run command with &&

--- dockerfile/entrypoint.sh
+++ dockerfile/entrypoint.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-service sshd start
+service sshd start && \
 su - work <<EOF
 /home/work/orp/nginx/load_nginx.sh start
 /home/work/orp/php/sbin/php-fpm -y /home/work/orp/php/etc/php-fpm.conf
