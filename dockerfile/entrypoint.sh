dfaea20f1b8ac2fcf764e9a790b95db35767ec14
gethin.yan <gethin.yan@gmail.com>
2019-01-05 14:47:30 +0800

    modify Dockerfile

--- dockerfile/entrypoint.sh
+++ dockerfile/entrypoint.sh
@@ -3,5 +3,5 @@ chown -R work:work /home/work/ && \
 service sshd start && \
 su - work <<EOF
 /home/work/orp/nginx/load_nginx.sh start
-/home/work/orp/php/sbin/php-fpm -y /home/work/orp/php/etc/php-fpm.conf
+/home/work/orp/php/sbin/php-fpm -y /home/work/orp/php/etc/php-fpm.conf &
 EOF
